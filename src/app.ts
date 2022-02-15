71d07aa0a39e6cd2679e06894e1c47bc2389e635
giancarvalho <giandcarvalho@gmail.com>
2022-02-15 13:13:08 -0300

    add addresses controller

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import * as AddressesController from './controller/addresses.controller'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+app.post('/addresses',AddressesController.resolveDistances);
 
 app.get('/health', (req, res) => res.sendStatus(200));
 
---
+++ src/controller/addresses.controller.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from 'express';
+
+function resolveDistances(req: Request, res: Response){
+
+  return res.sendStatus(200);
+}
+
+export {
+  resolveDistances,
+}
