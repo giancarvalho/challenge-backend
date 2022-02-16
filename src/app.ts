bfbf61e16720b435575e74c0ecfa99b817f0395f
giancarvalho <giandcarvalho@gmail.com>
2022-02-16 18:11:12 -0300

    add procfile

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,13 @@
-import './setup'
-import express from 'express';
-import cors from 'cors';
-import * as AddressesController from './controller/addresses.controller'
+import "./setup";
+import express from "express";
+import cors from "cors";
+import * as AddressesController from "./controller/addresses.controller";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.post('/addresses',AddressesController.resolveDistances);
-
-
-
-app.get('/health', (req, res) => res.sendStatus(200));
+app.post("/addresses", AddressesController.resolveDistances);
+app.get("/health", (req, res) => res.sendStatus(200));
 
 export default app;
